b3e49e7d6f2ef8e67a877666bc0b07d544f7fd07
Sjoerd Lemkens <97674344+sjoerdlmkns@users.noreply.github.com>
2023-02-03 20:34:08 +0100

    feat: changed to hello world

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-    site: 'https://sjoerdlmkns.github.io',
-    base: '/portfolio-sjoerdlmkns',
-  })
+  site: 'https://sjoerdlmkns.github.io',
+  base: '/portfolio-sjoerdlmkns',
+  integrations: [tailwind()]
+});
